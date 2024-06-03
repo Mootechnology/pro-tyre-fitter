e17ad8352abc601edff5732bfe99eec8cc95c996
usama <ahmarkhan1233456@gmail.com>
2024-06-03 10:33:50 +0500

    "Refactored FAQs section to use react-bootstrap Accordion component, removed unused imports and components"

--- src/faqs.jsx
+++ src/faqs.jsx
@@ -1,16 +1,13 @@
 import React, { useEffect } from "react";
 import {
-  MDBBtn,
   MDBCol,
   MDBContainer,
-  MDBInput,
   MDBRow,
-  MDBTextArea,
 } from "mdb-react-ui-kit";
 import { Link } from "react-router-dom";
-import logo from "./assets/images/logo2.png";
 import { Helmet } from "react-helmet";
 import { useLocation } from "react-router-dom";
+import Accordion from 'react-bootstrap/Accordion';
 
 export default function WithContactForm() {
   const location = useLocation();
@@ -60,37 +57,41 @@ export default function WithContactForm() {
         </h5>
       </header>
 
+      
+
       <MDBContainer style={{ maxWidth: "1000px" }}>
         <h3 className="text-center mt-5" style={{ fontSize: "10rem" }}>
           <strong className="text-success">Frequently Asked Question</strong>
         </h3>
-        <section className="mt-5">
+        <section className="mt-5 mb-3">
           <MDBRow>
-            <MDBCol lg="6" md="12" className="mb-4">
-              <div>
-                <p className="mb-1">
+            {/* Accordian */}
+            <Accordion defaultActiveKey="0">
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>
+                  <p className="mb-1">
                   <strong className="text-success">
                     How often do I need car tyre maintenance?
                   </strong>
                 </p>
+                </Accordion.Header>
+                <Accordion.Body>
                 <p className="mb-1">
                   <p>
                     You need to check the health of your tyres once a month.
                   </p>
                 </p>
-                {/* <ul>
-                <li>list item 1</li>
-                <li>list item 2</li>
-                <li>list item 3</li>
-              </ul> */}
-              </div>
-
-              <div>
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="1">
+                <Accordion.Header>
                 <p className="mb-1">
                   <strong className="text-success">
                     Do you offer mobile tyre fitting at my home?
                   </strong>
                 </p>
+                </Accordion.Header>
+                <Accordion.Body>
                 <p className="mb-1">
                   <p>
                     Yes, we provide mobile tyre fitting service at your home.
@@ -98,91 +99,110 @@ export default function WithContactForm() {
                     toolkit.
                   </p>
                 </p>
-              </div>
-
-              <div>
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="2">
+                <Accordion.Header>
                 <p className="mb-1">
                   <strong className="text-success">
                     Can you recommend the best tyres for my vehicle?
                   </strong>
                 </p>
+                </Accordion.Header>
+                <Accordion.Body>
                 <p className="mb-1">
                   <p>
                     Yes, we recommend the best tyres according to your vehicle
                     to make your journey easier.
                   </p>
                 </p>
-              </div>
-
-              <div>
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="3">
+                <Accordion.Header>
                 <p className="mb-1">
                   <strong className="text-success">
                     How long does it take you to replace a tyre?
                   </strong>
                 </p>
+                </Accordion.Header>
+                <Accordion.Body>
                 <p className="mb-1">
                   <p>
                     Our professionals are good at service; it takes 30 minutes
                     to change your old tyre to a new one.
                   </p>
                 </p>
-              </div>
-
-              <div>
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="4">
+                <Accordion.Header>
                 <p className="mb-1">
                   <strong className="text-success">
                   Can a mobile tyre fitter do a wheel alignment?
                   </strong>
                 </p>
+                </Accordion.Header>
+                <Accordion.Body>
                 <p className="mb-1">
                   <p>
                   Yes, a mobile tyre fitters make sure wheel alignment issues with manage other tyre related issues
                   </p>
                 </p>
-              </div>
-
-              <div>
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="5">
+                <Accordion.Header>
                 <p className="mb-1">
                   <strong className="text-success">
                     How do I protect my tyres from damage?
                   </strong>
                 </p>
+                </Accordion.Header>
+                <Accordion.Body>
                 <p className="mb-1">
                   <p>
                     Regular maintenance, maintaining tyre pressure and avoiding
                     bad driving habits can protect your tyres from damage.
                   </p>
                 </p>
-              </div>
-
-              <div>
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="6">
+                <Accordion.Header>
                 <p className="mb-1">
                   <strong className="text-success">
                   Is mobile tyre expensive?
                   </strong>
                 </p>
+                </Accordion.Header>
+                <Accordion.Body>
                 <p className="mb-1">
                   <p>
                   Mobile tyre fitting service is less expensive than garage based service. It is also protected against physical stress.
                   </p>
                 </p>
-              </div>
-
-              <div>
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="7">
+                <Accordion.Header>
                 <p className="mb-1">
                   <strong className="text-success">
                   How many miles does a tyre cover?
                   </strong>
                 </p>
+                </Accordion.Header>
+                <Accordion.Body>
                 <p className="mb-1">
                   <p>
                   Front tyres cover 20,000 miles and rear tyres 40,000 miles, further factors such as weather and maintenance also affect performance.
                   </p>
                 </p>
-              </div>
-
-            </MDBCol>
-            <MDBCol lg="6" md="12" className="text-center mb-3">
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+            {/* Accordian */}
+            {/* <MDBCol lg="6" md="12" className="text-center mb-3">
               <p>
                 <span class="fw-bold" className="text-success">
                   Still have any questions? Contact us to get your answer!
@@ -200,7 +220,7 @@ export default function WithContactForm() {
                   Send
                 </MDBBtn>{" "}
               </form>
-            </MDBCol>
+            </MDBCol> */}
           </MDBRow>
         </section>
       </MDBContainer>
